149687b4ca72d0493b5ee8cb23e88af84236ccc3
alexbanar <alex.banar10@gmail.com>
2020-11-14 22:36:23 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,11 +1,33 @@
 
-def test_scores_service(app_url):
+from selenium import webdriver
 
-    return
+def test_scores_service(app_url):
+   # options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True)
+    # chrome_driver = webdriver.Chrome(chrome_options=options, executable_path=r'D:\\Python\\PycharmProjects\\ChromeDriver.exe')
+
+    global chrome_driver
+    options = webdriver.ChromeOptions()
+    # options.add_experimental_option("detach", True)
+    options.add_argument("--window-size=480,320")
+    chrome_driver = webdriver.Chrome(options=options, executable_path="D:\\Python\\PycharmProjects\\ChromeDriver.exe")
+    chrome_driver.get(app_url)
+    chrome_driver.implicitly_wait(10)
+    main_scores = int(chrome_driver.find_element_by_id("score").text)
+    if main_scores >= 0 and main_scores <= 1000:
+        return True
+    else:
+        return False
 
 def main_function():
-    app_url = input("Please enter application URL..")
-    OS_exit_code = test_scores_service(app_url)
+    app_url = "http://127.0.0.1:8777"
+    scores_num_between_0_1000 = test_scores_service(app_url)
+
+    if scores_num_between_0_1000:
+        OS_exit_code = 0
+    else:
+        OS_exit_code = -1
+
     return OS_exit_code
 
 main_function()
