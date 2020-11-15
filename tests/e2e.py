e67cb71b30bb3c93c3f6a306764e228d2d302b63
alexbanar <alex.banar10@gmail.com>
2020-11-15 05:50:17 +0200

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -11,7 +11,7 @@ def test_scores_service(app_url):
     options = webdriver.ChromeOptions()
     # options.add_experimental_option("detach", True)
     options.add_argument("--window-size=480,320")
-    chrome_driver = webdriver.Chrome(options=options, executable_path=.\\chromedriver.exe')
+    chrome_driver = webdriver.Chrome(options=options, executable_path='.\\chromedriver.exe')
     chrome_driver.get(app_url)
     chrome_driver.implicitly_wait(10)
     main_scores = int(chrome_driver.find_element_by_id("score").text)
